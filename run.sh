b518ba8885797bd00aba4f6b6056d1961de9b82c
Emmanuel Chang <changeemma@gmail.com>
2022-01-23 06:18:04 -0300

    add first script using dmenu

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+#
+selection=""
+options=$(python main.py)
+
+# shellcheck disable=SC2236
+while [[ ! -z "$options" ]]; do
+    # select
+    s=$(echo "$options" | dmenu)
+
+    # append selection
+    selection="$selection $s"
+
+    # get options
+    # shellcheck disable=SC2086
+    options=$(python main.py $selection)
+done
