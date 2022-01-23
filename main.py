e96dd2bd9be5de9ccaf6e061b028b7aaed42a235
Emmanuel Chang <echang@santandertecnologia.com.ar>
2022-01-23 05:50:44 -0300

    refactor using tree-like class objects

--- main.py
+++ main.py
@@ -2,33 +2,80 @@ import argparse
 import pyperclip
 
 
-DATA = {
-    "reddit": {"naerokeht": "redditpass"},
-    "github": {"changeemma": "ghpass"},
-    "gmail": {
-        "changeemma@gmail.com": "gmpass",
-        "echang.dev@gmail.com": "fipass",
-    }
-}
+class ArchiveTree:
+    def __init__(self, name: str):
+        self.name = name
+        self.nodes = []
+
+    def __str__(self):
+        return self.name
+
+    def add_node(self, node):
+        self.nodes.append(node)
+
+    def is_named(self, name: str):
+        return self.name == name
+
+    def browse(self, name: str):
+        for node in self.nodes:
+            if node.is_named(name):
+                return node
+        raise KeyError
+
+    def open(self):
+        print('\n'.join([str(n) for n in self.nodes]))
 
 
-def main(options):
-    archive = DATA
-    for opt in options:
-        archive = archive[opt]
+class ArchiveLeaf:
+    def __init__(self, name: str, content: str):
+        self.name = name
+        self.content = content
 
-    if isinstance(archive, dict):
-        print('\n'.join(archive.keys()))
-    else:
-        pyperclip.copy(archive)
+    def __str__(self):
+        return self.name
+
+    def is_named(self, name: str):
+        return self.name == name
+
+    def browse(self, name):
+        return self
+
+    def open(self):
+        pyperclip.copy(self.content)
+
+
+reddit_node = ArchiveTree("reddit")
+reddit_node.add_node(ArchiveLeaf("naerokeht", "redditpass"))
+
+github_node = ArchiveTree("github")
+github_node.add_node(ArchiveLeaf("changeemma", "githubpass"))
+
+gmail_node = ArchiveTree("gmail")
+gmail_node.add_node(ArchiveLeaf("changeemma@gmail.com", "gmpass1"))
+gmail_node.add_node(ArchiveLeaf("echang.dev@gmail.com", "gmpass2"))
+
+ARCHIVE = ArchiveTree("archive")
+ARCHIVE.add_node(reddit_node)
+ARCHIVE.add_node(github_node)
+ARCHIVE.add_node(gmail_node)
+
+
+def main(path):
+    # traverse archive
+    archive = ARCHIVE
+    for p in path:
+        archive = archive.browse(p)
+
+    # open archive
+    archive.open()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='This is a description.')
-    parser.add_argument('options',
+    parser.add_argument('path',
                         metavar='node',
                         type=str,
                         nargs='*',
                         help='this is help')
     args = parser.parse_args()
-    main(args.options)
+    main(args.path)
